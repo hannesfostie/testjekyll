37aad246380b58fec191b4043e55eb6c4cf175d6
Hannes Fostie <hannes@openminds.be>
2013-10-01 20:29:32 +0200

    More deploy fixes

---
+++ config/deploy.rb
@@ -0,0 +1,6 @@
+set :application, "default"
+set :repository,  "git@github.com:hannesfostie/oneperfectline.git"
+set :branch,      "master"
+
+role :web, "oneperfectline@web-001.maloik.co"
+role :app, "oneperfectline@web-001.maloik.co"
